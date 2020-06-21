dc2bcaa37e4d5fcd815dd98db5aef94650c085e7
preethisamala <preethi.s16@iiits.in>
2020-06-21 23:08:58 +0530

    Updates

---
+++ precache-manifest.46f5c70acfbeb8055639eeb6dc98e934.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b265e26a8cfbad38fe957863dd2521d2",
+    "url": "/course_catalog/index.html"
+  },
+  {
+    "revision": "1ae67f25c0e3a10cda5a",
+    "url": "/course_catalog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "77d019e2a8cdbf59e5aa",
+    "url": "/course_catalog/static/css/main.4a11aa19.chunk.css"
+  },
+  {
+    "revision": "1ae67f25c0e3a10cda5a",
+    "url": "/course_catalog/static/js/2.dadf38c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course_catalog/static/js/2.dadf38c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d019e2a8cdbf59e5aa",
+    "url": "/course_catalog/static/js/main.ec4034fc.chunk.js"
+  },
+  {
+    "revision": "91701829930c846541b2",
+    "url": "/course_catalog/static/js/runtime-main.2279706e.js"
+  },
+  {
+    "revision": "ef638ae02100d0bb6931092cc007e009",
+    "url": "/course_catalog/static/media/calendar.ef638ae0.svg"
+  },
+  {
+    "revision": "6e03389a46fee3e1f738d1894ece678a",
+    "url": "/course_catalog/static/media/circle.6e03389a.svg"
+  },
+  {
+    "revision": "b600fb3fb66bba5d94e13e5faa66859e",
+    "url": "/course_catalog/static/media/info.b600fb3f.svg"
+  },
+  {
+    "revision": "685343e85d72bce2f70dff9c0d4fd56e",
+    "url": "/course_catalog/static/media/radio.685343e8.svg"
+  },
+  {
+    "revision": "68ae73a31ee1cf43c2761c9404ec73c2",
+    "url": "/course_catalog/static/media/radioActive.68ae73a3.svg"
+  },
+  {
+    "revision": "4d8ce2255499fae0e58d7052938b3599",
+    "url": "/course_catalog/static/media/search.4d8ce225.svg"
+  }
+]);
